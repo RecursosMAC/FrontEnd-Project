f32d4b6eb4dd940b2be7025e98696c65a4698850
Monserrat Gpe Del Angel Santiago <168608932+MonserratSantiago8@users.noreply.github.com>
2026-04-28 14:17:29 +0000

    Test

---
+++ proyecto_studylink/app/Menu/Menu.tsx
@@ -0,0 +1,10 @@
+
+import styles from './Menu.module.css'
+export default function Menu(){
+    return(
+        <header className={styles.header}>
+            <div>Esto es un header</div>
+        </header>
+    )
+}
+
