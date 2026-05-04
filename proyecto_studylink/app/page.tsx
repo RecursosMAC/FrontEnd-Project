b9357fe237f5830fe8b3bada078b7953cf8024f5
Angel0017 <89827655+Angel0017@users.noreply.github.com>
2026-05-04 02:41:15 +0000

    Creacion de barra

--- proyecto_studylink/app/page.tsx
+++ proyecto_studylink/app/page.tsx
@@ -1,9 +1,18 @@
-import Menu from "./Menu/Menu"
+import Menu from "./Menu/Menu";
+import Barra from "./Barra/barra";
 
 export default function HomePage() {
   return (
     <div style={{ display: "flex", minHeight: "100vh" }}>
       <Menu />
+
+      <div style={{ flex: 1 }}>
+        <Barra />
+
+        <main style={{ padding: "20px" }}>
+          <h1>Bienvenido</h1>
+        </main>
+      </div>
     </div>
   );
 }
