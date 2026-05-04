218ee0c187e9f7f5799e2518064206f17e98d343
Emiliano Ruiz Garcia <32943533+Emiliano1502@users.noreply.github.com>
2026-05-04 02:41:34 +0000

    Cambios al grid

--- proyecto_studylink/app/page.tsx
+++ proyecto_studylink/app/page.tsx
@@ -1,27 +1,5 @@
-import Tarjeta from "./Tarjetas/tarjetas";
+import Grid from "./grid/grid";
 
-export default function HomePage() {
-  return (
-    <div style={{ display: "flex", gap: "20px" }}>
-
-      <Tarjeta
-        tipo="pdf"
-        titulo="Algoritmos de Ordenamiento"
-        carrera="Ing. en Computación"
-        materia="Estructuras de Datos"
-        fecha={new Date(2025, 0, 14)}
-        autor="Ana García Martínez"
-      />
-
-      <Tarjeta
-        tipo="video"
-        titulo="Cálculo Integral"
-        carrera="Matemáticas"
-        materia="Cálculo II"
-        fecha={new Date(2025, 1, 10)}
-        autor="Juan Pérez"
-      />
-
-    </div>
-  );
+export default function Home() {
+  return <Grid />;
 }
