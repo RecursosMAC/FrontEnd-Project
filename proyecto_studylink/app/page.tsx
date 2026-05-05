04c9c533926635c5ad186d99da4ca91e2563c5cd
Emiliano Ruiz Garcia <32943533+Emiliano1502@users.noreply.github.com>
2026-05-05 14:52:00 +0000

    Grid 2

--- proyecto_studylink/app/page.tsx
+++ proyecto_studylink/app/page.tsx
@@ -1,37 +1,17 @@
 import Grid from "./grid/grid";
+import Tarjeta from "./Tarjetas/tarjetas";
 import Menu from "./Menu/Menu";
 import Barra from "./Barra/barra";
-import Tarjeta from "./Tarjetas/tarjetas";
+
 
 export default function HomePage() {
   return (
     <div style={{ display: "flex", minHeight: "100vh" }}>
       <Menu />
       
-      <Grid />;
-      
       <div style={{ flex: 1 }}>
         <Barra />
-
-        <main style={{ padding: "20px", display: "flex", gap: "20px" }}>
-          <Tarjeta
-            tipo="pdf"
-            titulo="Algoritmos de Ordenamiento"
-            carrera="MAC"
-            materia="Estructuras de Datos"
-            fecha={new Date(2025, 0, 14)}
-            autor="Ana García Martínez"
-          />
-
-          <Tarjeta
-            tipo="video"
-            titulo="Cálculo Integral"
-            carrera="Matemáticas"
-            materia="Cálculo II"
-            fecha={new Date(2025, 1, 10)}
-            autor="Juan Pérez"
-          />
-        </main>
+        <Grid />
       </div>
     </div>
   );
