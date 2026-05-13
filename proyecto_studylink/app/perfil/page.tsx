21bb81bf8d313e302defe65023b6d5f574153765
Estefani Michalle Dominguez Lira <321339968@pcpuma.acatlan.unam.mx>
2026-05-13 01:28:06 +0000

    Modulo de HeaderPerfil creado

---
+++ proyecto_studylink/app/perfil/page.tsx
@@ -0,0 +1,40 @@
+import Menu from "../Menu/Menu";
+import Barra from "../Barra/barra";
+import HeaderPerfil from "./HeaderPerfil/HeaderPerfil";
+// import Estadisticas from "./Estadisticas/Estadisticas";
+// import Informacion from "./Informacion/Informacion";
+// import Actividad from "./Actividad/Actividad";
+
+export default function PerfilPage() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        minHeight: "100vh",
+        background: "#f3f4f6",
+      }}
+    >
+      
+      <Menu />
+
+      <div
+        style={{
+          flex: 1,
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
+        
+        <Barra />
+
+        <HeaderPerfil />
+
+        
+
+        
+
+      </div>
+
+    </div>
+  );
+}
