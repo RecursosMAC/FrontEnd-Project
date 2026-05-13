8288927110ed8f17f8aa513574e3c261eb56de7b
Estefani Michalle Dominguez Lira <321339968@pcpuma.acatlan.unam.mx>
2026-05-13 02:07:37 +0000

    Page.tsx del apartado perfil actualizada

--- proyecto_studylink/app/perfil/page.tsx
+++ proyecto_studylink/app/perfil/page.tsx
@@ -1,11 +1,9 @@
 import Menu from "../Menu/Menu";
 import Barra from "../Barra/barra";
-
 import HeaderPerfil from "./HeaderPerfil/HeaderPerfil";
 import Informacion from "./Informacion/Informacion";
 import Asesor from "./Asesor/Asesor";
 import Estadisticas from "./Estadisticas/Estadisticas";
-// import Actividad from "./Actividad/Actividad";
 
 export default function PerfilPage() {
   return (
@@ -35,7 +33,6 @@ export default function PerfilPage() {
 
         <Estadisticas />
 
-   
       </div>
     </div>
   );
